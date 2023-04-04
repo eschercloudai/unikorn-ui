5723c0b98312a58e101b4472971a7b5f347b07e7
Simon Murray <spjmurray@yahoo.co.uk>
2023-04-04 11:36:57 +0100

    Auto Upgrade (#66)
    Allow CL and CP resources to be created with an aut-upgrade facility.

--- src/lib/CreateClusterModal.svelte
+++ src/lib/CreateClusterModal.svelte
@@ -28,6 +28,7 @@
 	import SelectField from '$lib/SelectField.svelte';
 	import CheckBoxField from '$lib/CheckBoxField.svelte';
 	import SliderField from '$lib/SliderField.svelte';
+	import TimeWindowField from '$lib/TimeWindowField.svelte';
 
 	// clusters allows name uniqueness checking.
 	export let clusters;
@@ -102,6 +103,19 @@
 	// Cluster versioning support.
 	let applicationBundles = [];
 	let applicationBundle = null;
+
+	let autoUpgrade = false;
+	let autoUpgradeDaysOfWeek = false;
+
+	let daysOfTheWeekWindows = {
+		Sunday: {},
+		Monday: {},
+		Tuesday: {},
+		Wednesday: {},
+		Thursday: {},
+		Friday: {},
+		Saturday: {}
+	};
 
 	// Add a new workload pool to the list.
 	function addPool() {
@@ -433,6 +447,33 @@
 			workloadPools: []
 		};
 
+		if (autoUpgrade) {
+			// Empty object means platform managed.
+			const aa = {};
+
+			if (autoUpgradeDaysOfWeek) {
+				let dow = {};
+
+				for (const [day, o] of Object.entries(daysOfTheWeekWindows)) {
+					if (!o.enabled) {
+						continue;
+					}
+
+					Object.defineProperty(dow, day.toLowerCase(), {
+						enumerable: true,
+						value: {
+							start: o.start,
+							end: o.end
+						}
+					});
+				}
+
+				aa.daysOfWeek = dow;
+			}
+
+			body.applicationBundleAutoUpgrade = aa;
+		}
+
 		if (keyPair) {
 			body.openstack.sshKeyName = keyPair.name;
 		}
@@ -540,6 +581,34 @@
 						bind:options={applicationBundles}
 						bind:value={applicationBundle}
 					/>
+
+					<CheckBoxField
+						id="autoUpgrade"
+						label="Enable auto-upgrade?"
+						help="Enables auto-upgrade of the cluster application bundle.  When checked the default setting will be to perform upgrades randomly from Monday-Friday 00:00-07:00 UTC.  This allows support to be be readily available in the rare event of disruption."
+						bind:checked={autoUpgrade}
+					/>
+
+					{#if autoUpgrade}
+						<section class="autoupgrade">
+							<CheckBoxField
+								id="autoUpgradeDaysOfWeek"
+								label="Enable auto-upgrade scheduling?"
+								help="The default auto-upgrade time-windows are recommended.  If this isn't suitable for your use case, this allows the days and time-windows to be manually specified."
+								bind:checked={autoUpgradeDaysOfWeek}
+							/>
+
+							{#if autoUpgradeDaysOfWeek}
+								{#each Object.keys(daysOfTheWeekWindows) as day}
+									<TimeWindowField
+										id="autoupgrade-{day.toLowerCase()}"
+										label={day}
+										bind:object={daysOfTheWeekWindows[day]}
+									/>
+								{/each}
+							{/if}
+						</section>
+					{/if}
 				</section>
 			</details>
 
---
+++ src/lib/TimeWindowField.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { timeOfDayFormatter } from '$lib/formatters.js';
+	import CheckBoxField from '$lib/CheckBoxField.svelte';
+	import SliderField from '$lib/SliderField.svelte';
+
+	export let id;
+	export let label;
+
+	export let enabled = false;
+	export let start = 0;
+	export let end = 7;
+
+	export let object;
+
+	$: {
+		object = {
+			enabled: enabled,
+			start: start,
+			end: end
+		};
+	}
+</script>
+
+<CheckBoxField {id} {label} help="Enables the selected time window" bind:checked={enabled} />
+
+{#if enabled}
+	<SliderField
+		id={`${id}-start`}
+		help="Time window start"
+		min="0"
+		max="23"
+		formatter={timeOfDayFormatter}
+		bind:value={start}
+	/>
+
+	<SliderField
+		id={`${id}-end`}
+		help="Time window end.  If smaller than the start time, the window will wrap into the next day.  If equal to the start time it will encompass the entire 24 hour period."
+		min="0"
+		max="23"
+		formatter={timeOfDayFormatter}
+		bind:value={end}
+	/>
+{/if}
