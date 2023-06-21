85ffef6c71fcd6c6100be9bcf62fc5b6c9154ef0
Simon Murray <spjmurray@yahoo.co.uk>
2023-06-21 13:21:01 +0100

    Version 0.1.24
    Seeing as we're at ground zero, remove any legacy code based on old
    versions.

--- src/lib/EditClusterModal.svelte
+++ src/lib/EditClusterModal.svelte
@@ -3,7 +3,6 @@
 	import { token, removeCredentials } from '$lib/credentials.js';
 	import { errors } from '$lib/errors.js';
 	import { createEventDispatcher } from 'svelte';
-	import { gte } from 'semver';
 
 	import {
 		listFlavors,
@@ -692,21 +691,19 @@
 						bind:checked={kubernetesDashboard}
 					/>
 
-					{#if gte(applicationBundle.version, '1.3.0')}
-						<CheckBoxField
-							id="file-storage"
-							label="Enable file storage?"
-							help="Enables POSIX file based persistent storage"
-							bind:checked={fileStorage}
-						/>
+					<CheckBoxField
+						id="file-storage"
+						label="Enable file storage?"
+						help="Enables POSIX file based persistent storage"
+						bind:checked={fileStorage}
+					/>
 
-						<CheckBoxField
-							id="prometheus"
-							label="Enable Prometheus?"
-							help="Enables the Prometheus operator that can be used to provide platform monitoring"
-							bind:checked={prometheus}
-						/>
-					{/if}
+					<CheckBoxField
+						id="prometheus"
+						label="Enable Prometheus?"
+						help="Enables the Prometheus operator that can be used to provide platform monitoring"
+						bind:checked={prometheus}
+					/>
 				</section>
 			</details>
 
